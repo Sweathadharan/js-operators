170d5af58d2546d0962c55a63f827ab25009f2b9
Sweathadharan <89176350+Sweathadharan@users.noreply.github.com>
2024-02-27 14:48:52 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-operators
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Sweathadharan/js-operators)
---
+++ script.js
@@ -0,0 +1,64 @@
+var x = 5;
+var y = 2;
+
+// Challenge 1: Perform the addition operation (add x and y) and print the answer in the console.
+console.log(x + y);
+// Challenge 2: Perform the Subtraction operation (subtract y from x) and print the answer in the console.
+console.log(x - y);
+// Challenge 3: Perform Multiplication operation (multiply x from y) and print the answer in console.
+console.log(x * y);
+// Challenge 4: Perform Division operation (Divide x by y) and print the answer in console.
+console.log(x / y);
+// Challenge 5: perform division operation (divide x by y) and print the remainder in console.
+console.log(x % y);
+// Challenge 6: Perform Exponent operation and print the answer in console.
+console.log(x ** y);
+var z = 20;
+
+// Challenge 7: calculate "x" to the power of the "y" and print the answer in console.
+console.log(Math.pow(x, y));
+// Challenge 8: Decrement the value of the variable "z" by 1 and print the z value in the console.
+console.log(--z);
+
+var a = 20;
+
+var b = 10;
+
+var c = 15;
+
+var d = 20;
+
+// Challenge 9: Perform Addition assignment operation to the variable a;
+a += 20;
+
+// Challenge 10: Perform 	Subtraction assignment operation to the variable b;
+b -= 10;
+// Challenge 11: Perform 	Multiplication assignment operation to the variable c;
+c *= 15;
+// Challenge 12: Perform 	Division assignment operation to the variable c;
+d /= 20;
+// Challenge 13: Perform 2 Strict equality Comparison operations, in which one should return true and one should return false.
+let s=10
+let t=10
+if(s===t)
+console.log(true)
+console.log(5==="5")
+
+// Challenge 14: Perform 2 Strict-non-equality Comparison operations, in which one should return true and one should return false.
+var cc=5
+console.log(2=="2")
+console.log((2+2)==5)
+
+
+// Challenge 15: Perform 2 Less than Comparison operations, in which one should return true and one should return false.
+console.log(3<5)
+console.log(10<2)
+// Challenge 16: Perform 2 Greater than Comparison operations, in which one should return true and one should return false.
+console.log(10>5)
+console.log(1>2)
+// Challenge 17: Perform 2 Less than or equal to Comparison operations, in which one should return true and one should return false.
+console.log(10<=10)
+console.log(20<=10)
+// Challenge 18: Perform 2 Greater than or equal to Comparison operations, in which one should return true and one should return false.
+console.log(5>=5)
+console.log(1>=2)
